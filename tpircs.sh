d04e21027d53a530aa10d05517a307332f54ece3
KevinMcK95 <KevinMcK95@shaw.ca>
2016-11-04 10:09:34 -0400

    tpircs.sh working fully

--- tpircs.sh
+++ tpircs.sh
@@ -4,7 +4,20 @@
 
 read -p "Insert a list of words: " -a list
 
-for i in $(seq 0 $((${#test[*]}-1)) ); do
-	echo -n "$i "
+declare -a finalList
 
+for i in $(seq 0 $((${#list[*]}-1)) ); do
+	result=0
+	length=${#list[$i]}
+	for j in $(seq 1 $length ) ; do
+		num=$(($length-1*j))
+		if (( $j == 1 )) ; then 
+			result=${list[$i]:$num:1}
+		else
+			result+=${list[$i]:$num:1}
+		fi
+	done
+	finalList[$i]="$result"
 done
+echo "Output: ${finalList[*]}"
+
