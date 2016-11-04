5df8a0430563a5b04c34edfb5d2477fc9c5dcade
KevinMcK95 <KevinMcK95@shaw.ca>
2016-11-04 00:03:42 -0400

    luckyNumber.sh seems to be up and running!

--- luckyNumber.sh
+++ luckyNumber.sh
@@ -2,8 +2,30 @@
 
 #Kevin McKinnon is the sole author of this work
 
+luckyNum() {
+num=$1
+total=0
 
-def luckyNum{
+for i in $(seq 0 $((${#num}-1)) ) ; do
+	int=${num:$i:1}
+	total=$(($total+$int))
+done
 
-
+if (( $total == 7 )); then
+	echo 0
+elif (( $total >= 10 )); then
+	echo $(luckyNum $total)
+else
+	echo 1
+fi
 }
+
+for test in $(seq 1000 10000); do
+
+	val=$(luckyNum $test)
+
+	if (($val == 0)); then
+		echo $test
+	fi
+
+done
